8c170155e10036e259d5046aa8c98e0488d991ca
Mohit <mohitjain6199@gmail.com>
2022-03-27 03:30:17 +0000

    restructure main script and add arguments

---
+++ src/arguments.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--algorithm', default='cnn', type=str)
+    parser.add_argument('--lr', default=1e-3, type=float)
+    parser.add_argument('--seed', default='0', type=str)
+    parser.add_argument('--exp_name', default='default', type=str)
+    parser.add_argument('--log_dir', default='logs', type=str)
+    parser.add_argument('--batch_size', default=32, type=int)
+
+    parser.add_argument('--overfit', default=0, type=int)
+    parser.add_argument('--data_root', default="../thermometers", type=str)
+
+    args = parser.parse_args()
+
+    assert args.seed is not None, 'must provide seed for experiment'
+    assert args.exp_name is not None, 'must provide an experiment suffix for experiment'
+    assert args.log_dir is not None, 'must provide a log directory for experiment'
+
+    return args
--- src/main.py
+++ src/main.py
@@ -10,16 +10,80 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from models.cnn import MyModel
 from logger import Logger
 from sklearn.metrics import accuracy_score
+from arguments import parse_args
+
+def evaluate(model, val_loader, L):
+    for img, target in val_loader:
+        img = img.cuda()
+        target = target.cuda()
+
+        prediction = model(img)
+
+        acc = accuracy_score(np.argmax(prediction.cpu().numpy(), axis=1), target.cpu().numpy())
+        val_loss = loss(prediction, target)
+
+        L.log("eval/loss", val_loss, epoch)
+        L.log("eval/acc", acc, epoch)
+
+def train(model, train_loader, val_loader, L, args):
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    loss = torch.nn.CrossEntropyLoss()
+    model.train()
+    start_time = time.time()
+
+    for epoch in range(10):
+        for img, target in train_loader:
+            img = img.cuda()
+            target = target.cuda()
+
+            prediction = model(img)
+            train_loss = loss(prediction, target)
+
+            L.log("train/loss", train_loss, epoch)
+
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+
+        L.log("train/duration", time.time() - start_time, epoch)
+        start_time = time.time()
+        L.dump(epoch)
+
+        with torch.no_grad():
+            model.eval()
+            evaluate(model, val_loader, L)
+            L.dump(epoch)
+            model.train()
+
+def test(model, test_loader):
+    test_acc, test_loss = 0.0, 0.0
+    for i, (img, target) in enumerate(test_loader):
+        img = img.cuda()
+        target = target.cuda()
+        prediction = model(img)
+
+        acc = accuracy_score(np.argmax(prediction.cpu().numpy(), axis=1), target.cpu().numpy())
+        _loss = loss(prediction, target)
+
+        test_acc += (1 / (i + 1)) * (test_acc - acc)
+        test_loss += (1 / (i + 1)) * (test_loss - _loss)
+
+    return test_loss, test_acc
 
 if __name__ == "__main__":
-    DATA_ROOT = "../thermometers"
-    print(DATA_ROOT)
-    dset = TemperatureDataset(DATA_ROOT, overfit=False)
+    args = parse_args()
 
-    if True:
+    DATA_ROOT = args.data_root
+    print(DATA_ROOT)
+    
+    dset = TemperatureDataset(DATA_ROOT, overfit=bool(args.overfit))
+    test_dset = TemperatureDataset(DATA_ROOT, train=False)
+
+    if args.overfit:
         v_split = 0.2
     else:
         v_split = 0.0
+
     dataset_size = len(dset)
     indices = list(range(dataset_size))
     split = int(np.floor(v_split * dataset_size))
@@ -30,62 +94,31 @@ if __name__ == "__main__":
     train_sampler = SubsetRandomSampler(train_indices)
     val_sampler = SubsetRandomSampler(val_indices)
 
-    train_loader = DataLoader(dset, batch_size=32, sampler=train_sampler)
-    val_loader = DataLoader(dset, batch_size=32, sampler=val_sampler)
+    train_loader = DataLoader(dset, batch_size=args.batch_size, sampler=train_sampler)
+    val_loader = DataLoader(dset, batch_size=args.batch_size, sampler=val_sampler)
+    test_loader = DataLoader(test_dset, batch_size=args.batch_size, shuffle=True)
 
     model = MyModel()
     model = model.cuda()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    #loss = torch.nn.MSELoss()
-    loss = torch.nn.CrossEntropyLoss()
-    print("Looping")
 
     work_dir = os.path.join("logs", "debug")
     print("Working Directory ", work_dir)
     utils.make_dir(work_dir)
     model_dir = utils.make_dir(os.path.join(work_dir, "model"))
-
     L = Logger(work_dir)
 
-    model.train()
-    start_time = time.time()
-    for epoch in range(10):
-        iters = 0
-        for img, target in train_loader:
-            iters += 1
-            img = img.cuda()
-            target = target.cuda()
+    print("MODEL FINISHED TRAINING!")
+    print("Saving Model!")
+    torch.save(
+        model.state_dict(), os.path.join(model_dir, "final_model.pt"))
+    print("\n")
+    print("Testing Model!!")
 
-            optimizer.zero_grad()
-            import pdb;
+    model.eval()
+    with torch.no_grad():
+        test_acc, test_loss = test(model, test_loader)
 
-            #pdb.set_trace()
-            prediction = model(img)
-            train_loss = loss(prediction, target)
-
-
-            L.log("train/loss", train_loss, epoch)
-
-            train_loss.backward()
-            optimizer.step()
-
-        L.log("train/duration", time.time() - start_time, epoch)
-        start_time = time.time()
-        L.dump(epoch)
-        with torch.no_grad():
-            model.eval()
-            for img, target in val_loader:
-                img = img.cuda()
-                target = target.cuda()
-                prediction = model(img)
-
-                acc = accuracy_score(np.argmax(prediction.cpu().numpy(), axis=1), target.cpu().numpy())
-
-                val_loss = loss(prediction, target)
-                L.log("eval/loss", val_loss, epoch)
-                L.log("eval/acc", acc, epoch)
-
-
-            L.dump(epoch)
-
-            model.train()
+    print("*"*25)
+    print("Test Loss: ", test_loss)
+    print("Test Acc: ", test_acc)
+    print("*" * 25)
